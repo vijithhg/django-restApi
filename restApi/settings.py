c096dc4deb98e1f48707c30e94b4b6454149a036
Vijith P <vijithhgedu@gmail.com>
2022-06-26 11:56:01 +0530

    Add files via upload

--- restApi/settings.py
+++ restApi/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1h8@@3)5%m&p5(o(cn3e_yp37s##j=z_-+*9d750vr#a3!45qe'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-djangoapi.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
